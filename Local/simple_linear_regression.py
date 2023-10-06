8baf994373aa2ce05f68a47b932545e4e67fd6b0
OtabekRizayev <otabek.rizayev@bk.ru>
2023-10-06 16:16:22 +0500

    added files

---
+++ Local/simple_linear_regression.py
@@ -0,0 +1,30 @@
+import numpy as np
+import pandas as pd
+
+df = pd.read_csv('data-sets/houses.csv', index_col=0)
+df.head()
+
+"""# Ustunlar ta'rifi
+- `location` - sotilayotgan uy manzili
+- `district` - uy joylashgan tuman
+- `rooms` - xonalar soni
+- `size` - uy maydoni (kv.m)
+- `level` - uy
+"""
+
+df['district'].value_counts()
+
+housing = df[df.district=='Чиланзарский']
+housing.head()
+
+X = housing['size']
+y = housing['price']
+
+theta1 = np.sum((X - np.mean(X)) * (y - np.mean(y))) / np.sum((X - np.mean(X)) ** 2)
+theta0 = np.mean(y) - theta1 * np.mean(X)
+print(f"Regression Model: y={theta0:.2f}, X={theta1:.2f}")
+
+X_test = 20
+y_pred = theta0 + theta1 * X_test
+print(f"Prediction(Bashorat): x={X_test}, y={y_pred:.2f}")
+print(f"Asl qiymat: y={X[X == X_test]}")
